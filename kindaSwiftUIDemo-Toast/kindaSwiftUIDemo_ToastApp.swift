d94f3f71bec5b30026de23add23febcf6e9ada4a
Alex Nagy <rebeloperalpar@gmail.com>
2023-04-03 12:30:11 +0300

    Initial Commit

---
+++ kindaSwiftUIDemo-Toast/kindaSwiftUIDemo_ToastApp.swift
@@ -0,0 +1,17 @@
+//
+//  kindaSwiftUIDemo_ToastApp.swift
+//  kindaSwiftUIDemo-Toast
+//
+//  Created by Alex Nagy on 03.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct kindaSwiftUIDemo_ToastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
